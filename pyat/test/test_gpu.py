2db94522296354a08e41dc2e31a8165ddf83ad9d
Jean-Luc Pons <pons@birra.esrf.fr>
2024-07-03 15:01:56 +0200

    Workflow test

--- pyat/test/test_gpu.py
+++ pyat/test/test_gpu.py
@@ -12,16 +12,6 @@ def hmba_lattice():
     return ring
 import numpy as np
 
-# Retrieve GPUs
-gpuList = at.tracking.gpu_info()
-nbGPU = len(gpuList)
-if nbGPU == 0:
-	sys.exit("No GPU found")
-
-# Display GPU info	
-print(f"{nbGPU} GPU detected")
-for g in gpuList:
-	print(g)
 
 # Perfrom a test
 ring = hmba_lattice()
